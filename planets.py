2824bcbc6053d1246951a2cb87499af58aa93697
Arto Päivinen <arto.pai@luukku.com>
2022-02-21 14:57:25 +0200

    Clock

--- planets.py
+++ planets.py
@@ -12,8 +12,10 @@ pygame.display.set_caption("Planet Simulation")
 
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False

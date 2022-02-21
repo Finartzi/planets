6e619524e70209fdb2cca9f783a6d71cf4ea0ae6
Arto Päivinen <arto.pai@luukku.com>
2022-02-21 18:39:43 +0200

    Moving objects

--- planets.py
+++ planets.py
@@ -41,14 +41,47 @@ class Planet:
         y = self.y * self.SCALE + HEIGHT / 2
         pygame.draw.circle(win, self.color, (x, y), self.radius)
 
+    def attraction(self, other):
+        # Distance between the objects
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+        if other.sun:
+            self.distance_to_sun = distance
+
+        # Force of attraction
+        force = self.G * self.mass * other.mass / distance**2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+        return force_x, force_y
+
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+        
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_velocity += total_fx / self.mass * self.TIMESTEP
+        self.y_velocity += total_fy / self.mass * self.TIMESTEP
+
+        self.x += self.x_velocity * self.TIMESTEP
+        self.y += self.y_velocity * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
     sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
-    sun.sun = True
-
+    sun.sun = True    
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
@@ -58,7 +91,7 @@ def main():
 
     while run:
         clock.tick(60)
-        #WIN.fill(WHITE)
+        WIN.fill((0,0,0))
         
 
         for event in pygame.event.get():
@@ -66,6 +99,7 @@ def main():
                 run = False
         
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
     
         pygame.display.update()
